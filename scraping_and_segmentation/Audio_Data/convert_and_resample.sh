f0cee29336e8526320b7ba3ec50ad1be7ea91433
jihopark <lastspurt17@gmail.com>
2018-08-13 15:46:58 +0800

    convert mp4 to wav again (make it mono

--- scraping_and_segmentation/Audio_Data/convert_and_resample.sh
+++ scraping_and_segmentation/Audio_Data/convert_and_resample.sh
@@ -9,9 +9,9 @@ echo processing directory: $DIRECTORY
 for d in $DIRECTORY/* ; do
     for child_name in $d/*; do
         shopt -s nullglob
-        for f in $child_name/total/*.mp4; do
+        for f in $child_name/sentences_audio/*.mp4; do
             filename="${f%.*}"
-            ffmpeg -i ${filename}.mp4 -ar 16000 ${filename}.wav
+            ffmpeg -i ${filename}.mp4 -ar 16000 -ac 1 ${filename}.wav
         done
     done
 done
